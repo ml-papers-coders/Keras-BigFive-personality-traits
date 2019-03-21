004205ff30f63388c049c8f8ec298fe4af39e5cb
Yosri Laifi <yosrilaifi@gmail.com>
2019-03-21 13:39:08 +0100

    utils

--- model.py
+++ model.py
@@ -2,12 +2,12 @@ from tensorflow.keras.models import Sequential,Model
 from tensorflow.keras.layers import Dense, Activation ,Conv2D,MaxPooling2D , Input,Maximum
 
 
-def BigFiveCnnModel(filter_shapes,pool_sizes,input_shape=(),filter_hs=[1,2,3],hidden_units=[200,200,2],conv_non_linear='relu'):
+def BigFiveCnnModel(filter_shapes,pool_sizes,input_shape=(312, 153, 300),filter_hs=[1,2,3],hidden_units=[200,200,2],conv_non_linear='relu'):
     """
     
     """
     feature_maps = hidden_units[0] #nb FM
-    model_input_layer= input(shape=input_shape)
+    model_input_layer= input(input_shape)
     layers=[]
     for i in range(len(filter_hs)):
         filter_shape = filter_shapes[i]
