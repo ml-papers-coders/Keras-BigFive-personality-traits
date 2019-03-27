42212658c1e725096c682604acea5bcf3d151a45
Yosri Laifi <yosrilaifi@gmail.com>
2019-03-27 09:16:52 +0100

    test

--- test_tfgenerator.py
+++ test_tfgenerator.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def test(attr):
     revs, W, W2, word_idx_map, vocab, mairesse ,charged_words=load_data(attr)
     datasets = w2idx(revs, word_idx_map, mairesse, charged_words, attr, max_l=149, max_s=312, filter_h=3)
-    dataset=tf.data.Dataset.from_generator(generator=tfgenerator,args=attr)
+    dataset=tf.data.Dataset.from_generator(generator=tfgenerator,output_types=(tf.int32,tf.float32,tf.int8),args=attr)
     iterator = dataset.make_one_shot_iterator()
     next_element = iterator.get_next()
     with tf.Session() as sess:
