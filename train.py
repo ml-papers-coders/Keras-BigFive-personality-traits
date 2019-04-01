27a5002ebad2dee1722598df7846999b927ee419
Yosri Laifi <yosrilaifi@gmail.com>
2019-04-01 11:30:43 +0200

    test

--- train.py
+++ train.py
@@ -95,8 +95,8 @@ def train(batch_size,attr=2,trainable_embed=False):
         generator=train_generator,
         epochs=10,
         validation_data=val_generator,
-        steps_per_epoch=steps//20
-        ,validation_steps=vsteps//20
+        steps_per_epoch=steps
+        ,validation_steps=vsteps
         ,callbacks=callbacks_list
         #,callbacks=[TensorBoard(
         #    log_dir=LOG_DIR, histogram_freq=0
